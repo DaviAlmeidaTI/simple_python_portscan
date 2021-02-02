5f9eb1cb00bea4c5d357984905b1e709cf238c4f
Davi Almeida <davicastroti@hotmail.com>
2021-02-01 23:51:06 -0300

    Mudanças em Interação e criação de Def

---
+++ pt-portscan.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import socket
+
+print(""" __   __  ___  ___      ___                 __   __   __  ___  __   __            
+|__) /  \  |  |__  |\ |  |  |  /\  |       |__) /  \ |__)  |  /__` /  `  /\  |\ | 
+|    \__/  |  |___ | \|  |  | /~~\ |___    |    \__/ |  \  |  .__/ \__, /~~\ | \| 
+                                      
+									                                              """)
+
+
+def scan():
+	ip = input("Digite o ip do alvo\n")
+
+	for ports in range(1,65535):
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		
+			if s.connect_ex((ip,ports)) == 0:
+
+				print ("PORTA", ports, "ABERTA")
+
+				s.close()
+
+
+scan()
