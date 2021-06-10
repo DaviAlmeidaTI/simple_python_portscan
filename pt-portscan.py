bdc602f50893460d6ae3d867ff7ef3e9366b7f0f
Davi Castro <davicastroti@hotmail.com>
2021-06-10 09:09:58 -0300

    added a success mensage

--- pt-portscan.py
+++ pt-portscan.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3.8
 import socket
-
-print(""" __   __  ___  ___      ___                 __   __   __  ___  __   __            
-|__) /  \  |  |__  |\ |  |  |  /\  |       |__) /  \ |__)  |  /__` /  `  /\  |\ | 
-|    \__/  |  |___ | \|  |  | /~~\ |___    |    \__/ |  \  |  .__/ \__, /~~\ | \| 
-                                      
-									                                              """)
-
-
 def scan():
-	ip = input("Digite o ip do alvo\n")
-
+    ip = input("Type the target IP") 
 	for ports in range(1,65535):
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # noqa: E731,E123
 		
 			if s.connect_ex((ip,ports)) == 0:
 
 				print ("PORTA", ports, "ABERTA")
 
-				s.close()
 
+				
+				s.close()
+	print("scanneamento concluido")
 
 scan()
